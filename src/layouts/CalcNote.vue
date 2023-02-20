930f4b741d814fcd69314e68c919256acab04ddf
Alan David Martínez <alan.david507@gmail.com>
2023-02-20 05:18:08 -0300

    alpha

---
+++ src/layouts/CalcNote.vue
@@ -0,0 +1,283 @@
+<template>
+  <!-- <q-layout view="lHh Lpr lFf"> -->
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex bg-image flex-center bg-grey-4">
+        <q-card
+          v-bind:style="$q.screen.lt.sm ? { width: '80%' } : { width: '40%' }"
+        >
+          <q-toolbar class="bg-green-9 text-center">
+            <q-toolbar-title>Calculadora de notas Presencial</q-toolbar-title>
+            <q-btn flat round dense @click="openGithub">
+              <q-tooltip class="bg-accent">vici0uz</q-tooltip>
+              <q-icon name="fa-brands fa-github" />
+            </q-btn>
+          </q-toolbar>
+          <q-card-section>
+            <div class="row">
+              <div class="text-center">
+                <div
+                  class="acumulado"
+                  :style="{ backgroundColor: tieneFirma ? 'green' : 'red' }"
+                  v-if="acumulado != 0"
+                >
+                  <h2>{{ acumulado }}</h2>
+                </div>
+                <div class="firmas" v-if="firmas > 0">
+                  {{ firmas }} <b>Firmas</b>
+                </div>
+              </div>
+              <!-- <div class="badge" v-if="firmas > 0">
+                <div class="text-center">
+                  <h3>
+                    <span>{{ firmas }}</span>
+                  </h3>
+                  <p>Firmas</p>
+                </div>
+              </div> -->
+            </div>
+          </q-card-section>
+          <q-card-section>
+            <q-form @reset="onReset" class="q-gutter-md" ref="formRef">
+              <!-- <q-card-section> -->
+              <div class="row">
+                <q-input
+                  v-model="pesoExamenes"
+                  type="number"
+                  label="Peso examenes"
+                  :rules="[
+                    (val) => !!val || 'Requerido',
+                    (val) => Number(val) <= 100 || 'No puede ser mayor a 100'
+                  ]"
+                  prefix="%"
+                  clearable
+                  dense
+                ></q-input>
+              </div>
+              <div class="row">
+                <q-input
+                  v-model="primerParcial"
+                  type="number"
+                  label="Primer parcial"
+                  :rules="[
+                    (val) => !!val || 'Requerido',
+                    (val) => Number(val) <= 100 || 'No puede ser mayor a 100'
+                  ]"
+                  prefix="%"
+                  clearable
+                  dense
+                />
+                <q-space></q-space>
+                <q-input
+                  v-model="segundoParcial"
+                  type="number"
+                  label="Segundo parcial o recuperatorio"
+                  :rules="[
+                    (val) => !!val || 'Requerido',
+                    (val) => Number(val) <= 100 || 'No puede ser mayor a 100'
+                  ]"
+                  prefix="%"
+                  clearable
+                  dense
+                ></q-input>
+              </div>
+              <!-- </q-card-section> -->
+              <!-- <q-card-section> -->
+              <q-toggle
+                size="70px"
+                color="green"
+                v-model="tieneTP"
+                val="true"
+                label="Trabajo Práctico"
+              />
+
+              <q-toggle
+                size="70px"
+                color="green"
+                v-model="tieneParticipacion"
+                val="true"
+                label="Participación"
+              />
+
+              <div class="row" v-if="tieneTP">
+                <q-input
+                  type="number"
+                  v-model="pesoTP"
+                  label="Peso trabajo práctico"
+                  prefix="%"
+                  clearable
+                  dense
+                  :rules="[
+                    (val) => (tieneTP && !!val) || 'Requerido',
+                    (val) => Number(val) <= 100 || 'No puede ser mayor a 100'
+                    // (val) => Number(val) < 100 || 'No puede ser mayor a 100'
+                  ]"
+                />
+                <q-space />
+                <q-input
+                  type="number"
+                  v-model="trabajoPractico"
+                  label="Trabajo práctico"
+                  prefix="%"
+                  v-if="tieneTP"
+                  clearable
+                  dense
+                  :rules="[
+                    (val) => (tieneTP && !!val) || 'Requerido',
+                    (val) => Number(val) <= 100 || 'No puede ser mayor a 100'
+                    // (val) => Number(val) < 100 || 'No puede ser mayor a 100'
+                  ]"
+                />
+              </div>
+              <div class="row" v-if="tieneParticipacion">
+                <q-input
+                  type="number"
+                  v-model="pesoParticipacion"
+                  label="Peso Participación"
+                  prefix="%"
+                  clearable
+                  dense
+                  :rules="[
+                    (val) => (tieneParticipacion && !!val) || 'Requerido',
+                    (val) => Number(val) <= 100 || 'No puede ser mayor a 100'
+                  ]"
+                />
+                <q-space />
+
+                <q-input
+                  type="number"
+                  v-model="participacion"
+                  label="Participación"
+                  prefix="%"
+                  clearable
+                  dense
+                  :rules="[
+                    (val) => (tieneTP && !!val) || 'Requerido',
+                    (val) => Number(val) <= 100 || 'No puede ser mayor a 100'
+                    // (val) => Number(val) < 100 || 'No puede ser mayor a 100'
+                  ]"
+                />
+              </div>
+              <div class="flex-center flex">
+                <q-btn
+                  label="Reset"
+                  type="reset"
+                  color="primary"
+                  flat
+                  class="q-ml-sm"
+                  icon="mail"
+                />
+              </div>
+            </q-form>
+          </q-card-section>
+        </q-card>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const pesoExamenes = ref(0);
+const pesoTP = ref(0);
+const pesoParticipacion = ref(0);
+
+const tieneFirma = ref(false);
+const tieneTP = ref(false);
+const tieneParticipacion = ref(false);
+
+const primerParcial = ref(0);
+const segundoParcial = ref(0);
+const trabajoPractico = ref(0);
+const participacion = ref(0);
+
+const acumulado = ref(0);
+const firmas = ref(0);
+const formRef = ref();
+
+const githubPro = 'https://github.com/vici0uz';
+
+function openGithub() {
+  window.open(githubPro, '_blank', 'noreferrer');
+}
+
+watch(
+  () => [
+    primerParcial.value,
+    segundoParcial.value,
+    pesoExamenes.value,
+    pesoParticipacion.value,
+    participacion.value,
+    pesoTP.value,
+    trabajoPractico.value
+  ],
+  () => {
+    let puntaje = 0;
+    if (pesoExamenes.value != 0) {
+      if (primerParcial.value != 0 && segundoParcial.value) {
+        puntaje =
+          ((Number(primerParcial.value) +
+            Number(segundoParcial.value.valueOf())) /
+            2) *
+          (pesoExamenes.value / 40);
+      }
+      if (tieneParticipacion.value) {
+        puntaje += (Number(participacion.value) * pesoParticipacion.value) / 40;
+      }
+      if (tieneTP.value) {
+        puntaje += (Number(trabajoPractico.value) * pesoTP.value) / 40;
+      }
+    }
+    acumulado.value = puntaje;
+    if (acumulado.value >= 50) {
+      tieneFirma.value = true;
+      firmas.value = 1;
+      if (acumulado.value >= 60) {
+        firmas.value = 2;
+      }
+    } else {
+      tieneFirma.value = false;
+      firmas.value = 0;
+    }
+  }
+);
+
+// function getAcumulado() {
+//   let score = 0;
+//   if (peso_examenes.value != 0) {
+//     let examenes = 0;
+//     if (primer_parcial.value != 0) {
+//       examenes + primer_parcial.value.valueOf();
+//     }
+//     if (segundo_parcial.value != 0) {
+//       examenes + segundo_parcial.value.valueOf();
+//     }
+//     score = examenes / 2;
+//   }
+//   return score;
+// }
+
+function onReset() {
+  pesoExamenes.value = 0;
+  primerParcial.value = 0;
+  segundoParcial.value = 0;
+}
+</script>
+<style lang="scss">
+.main {
+  border: 1px solid black;
+  // background-color: green;
+}
+.acumulado {
+  text-align: center;
+}
+.badge {
+  border-radius: 50%;
+  background-color: yellow;
+  width: 120px;
+  height: 120px;
+}
+.firmas {
+  background-color: yellow;
+}
+</style>
